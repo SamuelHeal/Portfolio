99336656ad5df2036e7085e8f1bfe9cddb06e65d
Samuel Heal <samuelheal@hotmail.com>
2021-06-22 15:51:01 +1000

    resume added

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,3 +7,22 @@ $(window).scroll(function(){
     $(".header").css("opacity", -1 + $(window).scrollTop() / 350)
     
   });
+
+const hamburger = document.querySelector(".menuBars");
+const navMenu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
